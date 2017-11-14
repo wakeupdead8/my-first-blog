98396240c7589b014c410127930d1c2c12d92200
Sergey <playboy4ik13@gmail.com>
2017-11-14 12:57:08 +0200

    Added some design

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import  include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^post/$', views.post, name='post'),
+    url(r'^contact/$', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
-def post_list(request):
+def index(request):
     return render(request, 'blog/index.html', {})
+
+def about(request):
+    return render(request, 'blog/about.html', {})
+
+def post(request):
+    return render(request, 'blog/post.html', {})
+
+def contact(request):
+    return render(request, 'blog/contact.html', {})
